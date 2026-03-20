a9a1d79a9e68b380ddd13e16bac924596356c575
thuydn-0171 <dang.nhu.thuy@sun-asterisk.com>
2026-03-20 21:22:02 +0900

    Add my-time.sh

---
+++ exercise-01-hello-world/my-time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Function to print the current date, time, and timezone
+print_current_time() {
+  echo "Current Date and Time: $(date '+%Y-%m-%d %H:%M:%S %Z')"
+}
+
+# Call the function
+print_current_time
